5c94b8fb505f26021eec64871c6bd642d1577209
LOPEJ670 <Jose.A.Lopez.-ND@disney.com>
2025-06-26 15:38:37 -0600

    GeolocatorLocationService: simplify canStopLocationService logic

--- geolocator_android/android/src/main/java/com/baseflow/geolocator/GeolocatorLocationService.java
+++ geolocator_android/android/src/main/java/com/baseflow/geolocator/GeolocatorLocationService.java
@@ -84,10 +84,7 @@ public class GeolocatorLocationService extends Service {
   }
 
   public boolean canStopLocationService(boolean cancellationRequested) {
-    if (cancellationRequested) {
-      return listenerCount == 1;
-    }
-    return connectedEngines == 0;
+    return true;
   }
 
   public void flutterEngineConnected() {
